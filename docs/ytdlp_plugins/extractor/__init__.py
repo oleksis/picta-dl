a4123f30e7b11f926faedbd9b5121e82dfce5049
oleksis <oleksis.fraga@gmail.com>
2021-05-18 12:58:28 -0400

    Update ytdlp_plugins extractor

--- docs/ytdlp_plugins/extractor/__init__.py
+++ docs/ytdlp_plugins/extractor/__init__.py
@@ -1,12 +1,2 @@
 # flake8: noqa
-import os
-import sys
-
-# Import extractors plugins if using Bundle App
-if hasattr(sys, "frozen"):
-    # for load_module extractor (plugins)
-    HERE = os.path.realpath(os.path.abspath(__file__))
-    sys.path.insert(0, os.path.dirname(HERE))
-
-# ⚠ Dont use relative import beyond top-level package
-from picta import PictaIE, PictaChannelPlaylistIE, PictaUserPlaylistIE  # type: ignore
+from .picta import PictaIE, PictaChannelPlaylistIE, PictaUserPlaylistIE
